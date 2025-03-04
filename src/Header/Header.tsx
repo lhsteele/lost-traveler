f50ff43a7f90349ddffb5915b68bebef8574a59f
Lisa Steele <lisa@walrus.ai>
2025-03-04 12:21:29 -1000

    Adds background images, Header, and parchment image

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react";
+import "./Header.css";
+
+const Header: FunctionComponent = () => {
+  return (
+    <div className="header">
+      <h1>- The Lost Traveler -</h1>
+    </div>
+  );
+};
+
+export default Header;
